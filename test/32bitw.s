9e91766ad6f2036932d40f3e58e5e92a0be6dc98
Fatiha Haider <fatiha@princeton.edu>
2025-10-05 13:49:00 -0400

    add sraw

--- test/32bitw.s
+++ test/32bitw.s
@@ -2,14 +2,14 @@
 # 32-BIT WORD INSTRUCTION TEST (RV64W)
 # ======================================================
 
-addiw x1, x0, 1
-slliw x1, x1, 5          # x1 = 0x100000000
-addiw x2, x1, 5          # low 32 bits + sign extend → x2 = 5
-slliw x3, x2, 2          # shift left 2 bits → x3 = 20
-srliw x4, x3, 1          # shift right logical → x4 = 10
-sraiw x5, x3, 2          # shift right arithmetic → x5 = 5
-subw x6, x3, x5          # 20 - 5 = 15
-sllw x7, x5, x2          # 5 << 5 = 160
-sraw x7, x5, x2
+addiw x1, x0, 1          # x1 = 1
+slliw x1, x1, 5          # x1 = 1 << 5 = 32
+addiw x2, x1, 5          # x2 = (32 + 5) = 37
+slliw x3, x2, 2          # x3 = 37 << 2 = 148
+srliw x4, x3, 1          # x4 = 148 >> 1 (logical) = 74
+sraiw x5, x3, 2          # x5 = 148 >> 2 (arith)   = 37
+subw  x6, x3, x5         # x6 = 148 - 37 = 111
+sllw  x7, x5, x2         # x7 = 37 << 37 = (shift by 5 bits mod 32) = 37 << 5 = 1184
+sraw  x7, x5, x2         # x7 = 37 >> 5 (arith) = 1
 
 .word 0xfeedfeed
