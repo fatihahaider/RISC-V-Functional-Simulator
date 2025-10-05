1093def7361b9e561e2baaa67a0629734a1615b7
Fatiha Haider <fatiha@princeton.edu>
2025-10-05 13:15:27 -0400

    edit files

--- test/32bitw.s
+++ test/32bitw.s
@@ -3,7 +3,7 @@
 # ======================================================
 
 addi x1, x0, 1
-slli x1, x1, 32          # x1 = 0x100000000
+slliw x1, x1, 5          # x1 = 0x100000000
 addiw x2, x1, 5          # low 32 bits + sign extend → x2 = 5
 slliw x3, x2, 2          # shift left 2 bits → x3 = 20
 srliw x4, x3, 1          # shift right logical → x4 = 10
