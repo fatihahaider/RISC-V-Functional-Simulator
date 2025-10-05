8db0afd5e54c44cc46e28bd42c08e4c7583b61cd
Fatiha Haider <fatiha@princeton.edu>
2025-10-05 17:09:00 -0400

    edit sign ext test

--- test/signextcheck.s
+++ test/signextcheck.s
@@ -14,8 +14,10 @@
 
 # --- I-TYPE SIGN EXTENSION ---
 addi x1, x0, -1          # x1 = 0xFFFFFFFFFFFFFFFF
-addi x2, x0, -2048       # x2 = 0xFFFFFFFFFFFFF800
-addi x4, x0, 4095        # x4 = 4095 (max 12-bit positive)
+addi x2, x0, 2047        # x2 = 0x7FF (max +12-bit)
+addi x3, x0, -2048       # x3 = 0xFFFFFFFFFFFFF800
+add  x4, x1, x2          # x4 = -1 + 2047 = 2046
+add  x5, x3, x2          # x5 = (-2048) + 2047 = -1
 
 # --- S-TYPE SIGN EXTENSION ---
 lui  x10, 0x1            # x10 = 0x1000
